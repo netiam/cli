b0fd6898f356e332d76280ab69116016998e9c24
Hannes Moser <hannes@impossiblearts.com>
2015-09-11 23:02:12 +0200

    feat: add init prompt

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,34 +7,30 @@ export default function(spec) {
   const {ui} = spec
 
   function run() {
-    return new Promise((resolve, reject) => {
-      ui.writeLine('Available commands in netiam-cli')
-      ui.writeLine('Version: ' + pkg.version)
+    ui.writeLine('Available commands in netiam-cli')
+    ui.writeLine('Version: ' + (pkg.version || '1.0.0-semantically-released'))
+    ui.writeLine('')
+
+    _.forEach(commands, command => {
+      const cmd = command()
+      ui.write('netiam ' + cmd.name)
+      if (cmd.anonymousOptions.length > 0) {
+        ui.write(' ' + chalk.yellow(cmd.anonymousOptions.join(' ')))
+      }
+      if (cmd.availableOptions.length > 0) {
+        ui.write(chalk.cyan(' <options…>'))
+      }
       ui.writeLine('')
 
-      _.forEach(commands, command => {
-        const cmd = command()
-        ui.write('netiam ' + cmd.name)
-        if (cmd.anonymousOptions.length > 0) {
-          ui.write(' ' + chalk.yellow(cmd.anonymousOptions.join(' ')))
-        }
-        if (cmd.availableOptions.length > 0) {
-          ui.write(chalk.cyan(' <options…>'))
-        }
-        ui.writeLine('')
+      ui.writeLine('  ' + cmd.description)
+      ui.writeLine('  ' + chalk.gray('aliases: ' + cmd.aliases.join(', ')))
 
-        ui.writeLine('  ' + cmd.description)
-        ui.writeLine('  ' + chalk.gray('aliases: ' + cmd.aliases.join(', ')))
-
-        if (cmd.availableOptions.length > 0) {
-          _.forEach(cmd.availableOptions, option => {
-            ui.writeLine('  ' + chalk.cyan('--' + option.name + ' (' + option.type.name + ') (Default: ' + option.default + ')'))
-            ui.writeLine('    ' + chalk.gray('aliases: ' + option.aliases.join(', ')))
-          })
-        }
-      })
-
-      resolve()
+      if (cmd.availableOptions.length > 0) {
+        _.forEach(cmd.availableOptions, option => {
+          ui.writeLine('  ' + chalk.cyan('--' + option.name + ' (' + option.type.name + ') (Default: ' + option.default + ')'))
+          ui.writeLine('    ' + chalk.gray('aliases: ' + option.aliases.join(', ')))
+        })
+      }
     })
   }
 
--- src/commands/init.js
+++ src/commands/init.js
@@ -1,14 +1,191 @@
+import inquirer from 'inquirer'
+import crypto from 'crypto'
+import fs from 'fs'
+import mkdirp from 'mkdirp'
+
 export default function init(spec) {
 
   function run() {
     return new Promise((resolve, reject) => {
-      resolve()
+      inquirer.prompt([
+        {
+          type: 'confirm',
+          name: 'authentication',
+          message: 'Need authentication?',
+          default: true
+        },
+        {
+          type: 'confirm',
+          name: 'authenticationFacebook',
+          message: 'Allow Sign In with Facebook?',
+          default: true,
+          when: answers => answers.authentication
+        },
+        {
+          type: 'input',
+          name: 'facebookClientId',
+          message: 'Client ID of your Facebook app',
+          when: answers => answers.authenticationFacebook
+        },
+        {
+          type: 'password',
+          name: 'facebookClientSecret',
+          message: 'Client Secret of your Facebook app',
+          when: answers => answers.authenticationFacebook
+        },
+        {
+          type: 'input',
+          name: 'secret',
+          message: 'Your API secret',
+          default: crypto.randomBytes(16).toString('hex')
+        },
+        {
+          type: 'confirm',
+          name: 'persistence',
+          message: 'Need peristance?',
+          default: true
+        },
+        {
+          type: 'list',
+          name: 'database',
+          choices: ['PostgreSQL', 'MySQL', 'MongoDB', 'Memory'],
+          message: 'Choose a database',
+          default: 'Memory',
+          when: answers => {
+            return answers.persistence
+          }
+        },
+        {
+          type: 'input',
+          name: 'databaseHost',
+          message: 'Database host',
+          default: 'localhost',
+          when: answers => {
+            if (!answers.persistence) {
+              return false
+            }
+            if (answers.database === 'Memory') {
+              return false
+            }
+            return true
+          }
+        },
+        {
+          type: 'input',
+          name: 'databasePort',
+          message: 'Database port',
+          default: answers => {
+            if (answers.database === 'PostgreSQL') {
+              return 5432
+            }
+            if (answers.database === 'MySQL') {
+              return 3306
+            }
+            if (answers.database === 'MongoDB') {
+              return 27017
+            }
+            return 10000
+          },
+          when: answers => {
+            if (!answers.persistence) {
+              return false
+            }
+
+            if (answers.database === 'Memory') {
+              return false
+            }
+            return true
+          }
+        },
+        {
+          type: 'input',
+          name: 'databaseUsername',
+          message: 'Database username',
+          default: 'netiam',
+          when: answers => {
+            if (answers.database === 'PostgreSQL') {
+              return true
+            }
+            if (answers.database === 'MySQL') {
+              return true
+            }
+            return false
+          }
+        },
+        {
+          type: 'password',
+          name: 'databasePassword',
+          message: 'Database password',
+          when: answers => {
+            if (answers.database === 'PostgreSQL') {
+              return true
+            }
+            if (answers.database === 'MySQL') {
+              return true
+            }
+            return false
+          }
+        },
+        {
+          type: 'input',
+          name: 'port',
+          message: 'Server port',
+          default: 3000
+        },
+        {
+          type: 'checkbox',
+          name: 'features',
+          message: 'Enable middleware and features',
+          choices: [
+            {
+              name: 'Compression (e.g. GZIP)',
+              value: 'compression'
+            },
+            {
+              name: 'Reverse Proxy (e.g. nginx)',
+              value: 'proxy'
+            },
+            {
+              name: 'Cluster',
+              value: 'cluster'
+            },
+            {
+              name: 'CORS',
+              value: 'cors'
+            },
+            {
+              name: 'ACL',
+              value: 'acl'
+            },
+            {
+              name: 'JSONAPI',
+              value: 'jsonapi'
+            },
+            {
+              name: 'Documentation',
+              value: 'documentation'
+            },
+            {
+              name: 'Dockerfile',
+              value: 'dockerfile'
+            },
+          ],
+          default: [
+            'compression',
+            'proxy',
+            'cors',
+            'documentation'
+          ]
+        }
+      ], answers => {
+        console.log(answers)
+      })
     })
   }
 
   return Object.freeze({
     name: 'init',
-    works: 'outsideProject',
+    works: 'insideProject',
     description: 'Creates a new netiam-cli project in the current folder.',
     aliases: ['i'],
 
